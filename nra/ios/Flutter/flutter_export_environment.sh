eebeb65c4c72e5372dd7b266cc2a6510d4dd4a71
shadowjeet <44452792+shadowjeet@users.noreply.github.com>
2019-11-24 20:19:13 +0100

    Add files via upload

---
+++ nra/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\api\systemtools\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\api\projects\nra"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\api\systemtools\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
