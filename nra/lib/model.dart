28dae53c2c7d27dde412c181708b414af05b752b
shadowjeet <44452792+shadowjeet@users.noreply.github.com>
2019-11-25 00:35:06 +0100

    Update model.dart

--- nra/lib/model.dart
+++ nra/lib/model.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
-
-
-
 class AccessTicketFile{
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
